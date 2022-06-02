2233cae0a0adf97de022e560077b65b9aea4e0ab
root <you@example.com>
2022-06-02 12:52:24 -0700

    git

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,13 @@
 *	Descripton: to print each size
 *
 *	Return: always 0
-*/ 
+*/
 int main(void)
 {
-	printf("size of a char: 1 byte(s)");
-	printf("size of an int: 4 byte(s)");
-	printf("size of a long int: 4 byte(s)");
-	printf("size of long long int: 8 byte(s)");
-	printf("size of a float: 4 byte(s)");
+	printf("size of a char: 1 byte(s)\n");
+	printf("size of an int: 4 byte(s)\n");
+	printf("size of a long int: 4 byte(s)\n");
+	printf("size of long long int: 8 byte(s)\n");
+	printf("size of a float: 4 byte(s)\n");
 	return (0);
 }
