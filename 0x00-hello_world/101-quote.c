6f094c3d840f98824c4e9042071e524f54c64600
root <you@example.com>
2022-06-02 13:23:28 -0700

    7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*	main - print other string
+*	Description: is print some string of word
+*	Return: always 0
+*
+*/
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
