08bf9670d4f9c7904b138f9ff8f2327ad6417c63
root <you@example.com>
2022-06-02 06:59:55 -0700

    git

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<main.h>
+#include<stdio.h>
+/**
+ * main
+ *
+ * return:always 0
+ */
+int main();
+{
+	char ch="programming is like building a multilingual puzzle";
+	put(ch);
+	return (0;)
