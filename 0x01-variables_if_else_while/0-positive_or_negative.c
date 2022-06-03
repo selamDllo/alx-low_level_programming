9defc88f8827a09b9cdbca577615df36e3db5501
root <you@example.com>
2022-06-03 08:46:36 -0700

    git

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/*
+*	main - print the number 
+*	Discription: print number is positive negative or zero
+*	Rreturn: always 0
+*/
+int main(void)
+{
+	int n;
+	if(n>0)
+	{
+	printf("is positive");
+	}
+	else if(n<0)
+	{
+	printf("is negative");
+	{
+	if(n=0)
+	{
+	printf("is zero");
+	}
+	return(0);
+}
