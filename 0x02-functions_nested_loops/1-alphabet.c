85402f819e9c6c027a98d74059b9f57f234adcdb
root <you@example.com>
2022-06-08 10:20:43 -0700

    git

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+*	main - print alphabet letter
+*	Description:p to rint letter of alphabet 
+*	Return: always 0
+*/
+void print_alphabet(void)
+{
+	string ch="abcdefghijklmnopqrstuvwxyz";
+	printf("ch\n");
+}
