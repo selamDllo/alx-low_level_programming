e44f8433323f34a0e43ebd87a464bb92584cfe73
root <you@example.com>
2022-06-08 00:29:47 -0700

    git

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*
+*	main - print putchar
+*	Description: to print the word putchar
+*
+*	Return: always zero
+*
+*/
+int main(void);
+{
+	printf("_putchar");
+	return (0);
+}
