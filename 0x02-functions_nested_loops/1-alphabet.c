26c9081e99639ce9101bc540e09eeef6a95c3006
root <you@example.com>
2022-06-08 10:54:42 -0700

    git

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-/*
+/**
 *	main - print alphabet letter
-*	Description:p to rint letter of alphabet 
+*	Description: print to rint letter of alphabet
 *	Return: always 0
 */
 void print_alphabet(void)
 {
 	int a;
-	for (a=97; a<=122; a++)
+
+	for (a=97; a <= 122; a++)
 	{
 		putchar(a);
 	}
