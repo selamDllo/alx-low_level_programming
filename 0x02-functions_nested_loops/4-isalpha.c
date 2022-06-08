b6de583be54adcc466886403303bc2859491d0b2
root <you@example.com>
2022-06-08 12:45:41 -0700

    fix

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+*	_isalpha - check the code
+*	@c: is letter
+*	Description: check the letter
+*
+*	Return (0);
+*/
+int _isalpha(int c)
+{
+	if(((c < 122) && (c > 97)) || ((c < 90) && (c>65)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
