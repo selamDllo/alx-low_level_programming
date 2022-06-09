b01c824569490868bd821ebf4d9f13282200d2e6
root <you@example.com>
2022-06-09 13:26:24 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include "main.h"
 /**
 *	main - tests function that prints if integer 
 *	Return: 0
 */
+
 int main(void)
 {
 	int i;
 
-	i=0;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
