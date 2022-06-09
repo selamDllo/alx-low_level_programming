ac1edbde7d0611e971aede47b8bc8a581230d52d
root <you@example.com>
2022-06-09 02:25:27 -0700

    git

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+*	mul - print multile of two number
+*	@a && @b: multiple both
+*	Description: multiple two number and thier result
+*	Return: always 0
+*/
+int mul(int a, int b)
+{
+	printf("%d\n", mul(a, b));
+	return (0);
+}
