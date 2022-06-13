420247753d9b0f83be6a2ffd0ef44690e7203b5e
root <you@example.com>
2022-06-13 09:10:03 -0700

    git

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,11 @@
 /**
 *	_puts - prints string
 *	@str: first number
-*	Description: print string 
+*	Description: print string
 *	Return: always 0
 */
 void _puts(char *str)
 {
 	_puts(str);
-
+	_puts('\n');
 }
