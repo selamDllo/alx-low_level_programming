31c5331fa144be0af326b3cce79fd3982dcfd58e
root <you@example.com>
2022-06-13 12:11:03 -0700

    git

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*	print_rev - print the reverse
+*	@s: dtring
+*	Description: to print the reverse
+*	Return: always 0
+*/
+void print_rev(char *s)
+{
+	int k;
+
+	int i;
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	for (k = i; k > 0; k--)
+	{
+		printf("%c\n", *(s + k));
+	}
+}
