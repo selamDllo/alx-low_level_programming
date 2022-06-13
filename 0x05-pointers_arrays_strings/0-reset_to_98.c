c932bc0e0a0bdded8fb07524cfb55a2fc5bf3d11
root <you@example.com>
2022-06-13 06:06:57 -0700

    git

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*	reset_to_98 - updates the pointer
+*	@*n: the content
+*	Description: to discribe pointer
+*	Return: always 0
+*/
+void reset_to_98(int *n)
+{
+	int k = 98;
+	n = & k;
+	printf("%d", *n);
+}
