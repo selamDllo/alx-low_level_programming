fc8a06dbfd67a1d1d85766b030815e9e7807fe31
root <you@example.com>
2022-06-14 09:27:05 -0700

    git

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+*	puts2 - print every character
+*	@a: string
+*	@n: integer
+*	Description: to print character
+*	Return: always 0
+*/
+void print_array(int *a, int n)
+{
+	int k;
+
+	while (*(a + n) != '\0')
+	{
+
+		n++;
+	}
+	for (k = 0; k < n; k++)
+	{
+		printf("%c, ", *(a + k));
+
+	}
+	printf("\n");
+}
