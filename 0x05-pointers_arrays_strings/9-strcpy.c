feccae8c129c85aa3e82f98c9efe18434ac776e5
root <you@example.com>
2022-06-14 12:59:16 -0700

    git

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,16 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	char temp;
-	temp = *dest;
-	*dest = *src;
-	*src = temp;
+	int i;
+
+	i = 0;
+
+	while (*(src + i) != '\0')
+	{
+
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = *(src + i);
 	return (dest);
 }
