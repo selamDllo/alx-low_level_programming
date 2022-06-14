65f00ee3918a2c6094184e8e0252c7ca68f6a1a1
root <you@example.com>
2022-06-14 07:33:41 -0700

    git

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+*	puts2 - print every character
+*	@str: string
+*	Description: to print character
+*	Return: always 0
+*/
+void puts2(char *str)
+{
+ 	int k;
+
+	int i;
+	
+	i = 0;
+	
+	while (i < '\0')
+	{
+
+		i++;
+	}
+	for ( k = 0; k<i; k++)
+	{
+		if (k%2 != 0)
+		{
+			printf("%c", *(str + k));
+		}
+	}
+}
