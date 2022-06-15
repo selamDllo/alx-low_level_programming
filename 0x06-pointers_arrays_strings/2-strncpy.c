4804e24c686f736c70e73773c98525fe63bea9c1
root <you@example.com>
2022-06-15 14:31:53 -0700

    git

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+/**
+*	*_strncpy - copies a string
+*	@dest: string
+*	@src: string
+*	@n: intger
+*	Description: copies a string
+*	Return: always 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int tmp;
+
+	tmp = *(dest + n);
+
+	*(dest + n) = *(src + n);
+
+	*(src + n) = tmp;
+	return (0);
+}
