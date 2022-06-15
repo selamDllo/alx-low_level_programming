a28dd3e924240a19d6c0305d3f38ba1c1ddc2281
root <you@example.com>
2022-06-15 12:40:31 -0700

    git

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+/**
+*	*_strncat - concatenate two string
+*	@dest: string
+*	@n: intger
+*	@src: string
+*	Description: concatenate two string
+*	Return:always 0
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	int k;
+
+	i = strlen(dest);
+	k = strlen(src);
+
+	if (n != '\0')
+	{
+		for (n = 0; n < k; n++)
+		{
+			*(dest + (i - 1) + n) = *(src + n);
+		}
+	}	
+
+	return (dest);
+}
+
