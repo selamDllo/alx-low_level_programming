1b5cf4898207ff30ab923555948da462b74aaba8
root <you@example.com>
2022-06-16 14:19:20 -0700

    fix

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*	reverse_array - copies a string
+*	@a: array
+*	Description: copies a string
+*/
+char *cap_string(char *a)
+{
+	char *res;
+
+	res = a;
+
+	while (*a != '\0')
+	{
+		if (*a == '\n' || *a == ',' || *a == ';' ||
+				*a == '.' || *a == '!' ||
+				*a == '?' || *a == '\"'
+				|| *a == '(' || *a == ')' ||
+				*a == '{' || *a == '}' ||
+				*a == ' ' || *a == '\t')
+		{
+			a++;
+			if (*a > 97 && *a < 122)
+			{
+				*a -= 32;
+			}
+			else if (*a == '\0')
+				break;
+		}
+		a++;
+	}
+	return (res);
+}
