b42262260ac1b3c66f3cfcb15c083f6f956d09a8
root <you@example.com>
2022-06-16 11:55:19 -0700

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<string.h>
+
 /**
-*	*_strncpy - copies a string
+*	_strncpy - copies a string
 *	@dest: string
 *	@src: string
 *	@n: intger
 *	Description: copies a string
 *	Return: always 0
 */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int tmp;
@@ -17,5 +19,5 @@ char *_strncpy(char *dest, char *src, int n)
 	*(dest + n) = *(src + n);
 
 	*(src + n) = tmp;
-	return (0);
+	return (dest);
 }
