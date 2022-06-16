a41d8b210807332ce8bf01bb1b9eaf5958fa9abc
root <you@example.com>
2022-06-16 11:59:40 -0700

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+
 /**
 *	*_strncat - concatenate two string
 *	@dest: string
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,59 +2,25 @@
 #include<string.h>
 /**
 *	_strcmp - copies a string
-*	@s1: string
-*	@s2: string
+*	@a: array
+*	@n: number
 *	Description: copies a string
 *	Return: always 0
 */
-int _strcmp(char *s1, char *s2)
+void reverse_array(int *a, int n)
 {
 	int l;
 
-	int smallest;
+	int tmp;
 
-	int k;
-
-	int m;
-
-	k = strlen(s1);
-	m = strlen(s2);
-
-	if (k < m)
+	for (l = 0; l > a.length/2; l++)
 	{
-		smallest = k;
-	}
-	else
-	{
-		smallest = m;
-	}
+		tmp = a[l];
 
-	for (l = 0; l < smallest; l++)
-	{
-		if (*(s1 + l) == *(s2 + l))
-		{
+		a[l] = a[a.length - l];
 
-		}
-		else if (*(s1 + l) > *(s2 + l))
-		{
-			return (15);
-		}
-		else if (*(s1 + l) < *(s2 + l))
-		{
-			return (-15);
-
-		}
-	}
-	if (k != m)
-	{
-		if (k < m)
-		{
-			return (-15);
-		}
-		else
-		{
-			return (15);
-		}
+		a[a.length - l] = tmp;
+		
 	}
 	return (0);
 }
