e4ad20306312eb4a7c3b6f59db717ea954ee52e4
root <you@example.com>
2022-06-17 04:00:07 -0700

    git

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-
 /**
-*	cap_string - copies a string
+*	*cap_string - copies a string
 *	@a: array
 *	Description: copies a string
 *	Return: the char pointer
@@ -17,13 +16,14 @@ char *cap_string(char *a)
 	while (*a != '\0')
 	{
 		if (*a == '\n' || *a == ',' || *a == ';' ||
-				*a == '.' || *a == '!' ||
-				*a == '?' || *a == '\"'
-				|| *a == '(' || *a == ')' ||
-				*a == '{' || *a == '}' ||
-				*a == ' ' || *a == '\t')
+			*a == '.' || *a == '!' ||
+			*a == '?' || *a == '\"'
+			|| *a == '(' || *a == ')' ||
+			*a == '{' || *a == '}' ||
+			*a == ' ' || *a == '\t')
 		{
 			a++;
+
 			if (*a > 97 && *a < 122)
 			{
 				*a -= 32;
