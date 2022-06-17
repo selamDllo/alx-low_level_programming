7cb362523a71f2a14320dfb4ea1cc9e3caa19853
root <you@example.com>
2022-06-16 23:38:55 -0700

    fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*	cap_string - copies a string
+*	@a: array
+*	Description: copies a string
+*	Return: the char pointer
+*/
+char *cap_string(char *a)
+{
+	char *res;
+
+	res = a;
+
+	while (*a != '\0')
+	{
+		if (*a == '\n' || *a == ',' || *a == ';' ||
+				*a == '.' || *a == '!' ||
+				*a == '?' || *a == '\"'
+				|| *a == '(' || *a == ')' ||
+				*a == '{' || *a == '}' ||
+				*a == ' ' || *a == '\t')
+		{
+			a++;
+			if (*a > 97 && *a < 122)
+			{
+				*a -= 32;
+			}
+			else if (*a == '\0')
+				break;
+		}
+		else
+			a++;
+	}
+	return (res);
+}
