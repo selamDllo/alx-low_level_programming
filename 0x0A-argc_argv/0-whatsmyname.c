568f37b3b61303044b5564591741cb4a965a7313
root <you@example.com>
2022-06-24 07:00:21 -0700

    git

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*	main - print name
+*	Description: to print name
+*	Return: always 0
+*/
+int main()
+{
+	char *name;
+
+	name = "./mynewnameis";
+
+	printf("%s\n" ,name);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*	main - print name
+*	@argc: number
+*	@argv: string
+*	Description: to print name
+*	Return: always 0
+*/
+int main (int argc, char *argv[])
+{
+
