a02e3d88c050c37a26d598e258008ec915ab6994
root <you@example.com>
2022-06-27 10:37:32 -0700

    git

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+*	main - print name
+*	@argc: integer
+*	@argv: integer
+*	Description: to print name
+*	Return: always 0
+*/
+
+int main(int argc, char **argv)
+{
+	int i, k, add;
+
+	char *c;
+
+	add = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* check all the input digits are numbers */
+	for (i = 1; i < argc; i++)
+	{
+		c = argv[i];
+
+		while (*c != '\0')
+		{
+			if ((*c < 48) || (*c > 57))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			c++;
+		}
+
+		k = atoi(argv[i]);
+		add *= k;
+	}
+
+	printf("%d\n", add);
+	return (0);
+}
+
