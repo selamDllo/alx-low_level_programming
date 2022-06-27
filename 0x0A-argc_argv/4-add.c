7f24a1c6bac51359fc7688cf49ffc3c97357accf
root <you@example.com>
2022-06-27 09:59:18 -0700

    git

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,51 +10,34 @@
 */
 int main(int argc, char **argv)
 {
-	int i;
-
-	int k;
-
-	int add;
+	int i, k, add;
+	char *c;
 
 	add = 0;
 
 	if (argc < 3)
 	{
 		printf("Error");
-
 		return (1);
 	}
-	
 	/* check all the input digits are numbers */
 	for (i = 1; i < argc; i++)
 	{
-		char *c = argv[i];
-		
+		c = argv[i];
 		while (*c != '\0')
 		{
 			if ((*c < 48) || (*c > 57))
 			{
 				printf("Error");
-				return (0);
+				return (1);
 			}
-
 			c++;
 		}
-
-	}
-	
-	for (i = 1; i < argc; i++)
-	{
-
 		k = atoi(argv[i]);
-		
 		add += k;
-
 	}
 
 	printf("%d\n", add);
-
 	return (0);
-
 }
 
