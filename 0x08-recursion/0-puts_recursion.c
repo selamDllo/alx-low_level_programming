ea22cf7bcec4cbe1bb5b49254599bd50347c9459
root <you@example.com>
2022-06-27 09:07:02 -0700

    git

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*	_puts_recursion - to print string
+*	@s: string
+*	Description: to print the function
+*	Return: always 0
+*/
+void _puts_recursion(char *s)
+{
+	printf("%c\n", *s);
+}
