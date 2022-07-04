37ec67f7c67c6acd1a1882638331b14d8b6ba603
root <you@example.com>
2022-07-04 11:39:10 -0700

    git

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
 *       Description: to practice structures
 *       Return: always 0
 */
-struct dog
+struct dog 
 {
 	char *name;
 
@@ -17,6 +17,7 @@ struct dog
 	char *owner;
 
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
