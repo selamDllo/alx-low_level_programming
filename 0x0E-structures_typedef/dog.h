f47e626c5ccc0c565dbc96eebb542e3614f219d5
root <you@example.com>
2022-07-04 08:55:19 -0700

    git

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*       struct dog - to practice structure
+*       @name: string
+*       @age: number
+*       @owner: string
+*       Description: to practice structures
+*       Return: always 0
+*/
+struct dog
+{
+        char *name;
+
+        float age;
+
+        char *owner;
+
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
