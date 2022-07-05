307c765c1b65bbf859ebccb2dff59b750e2d9c15
root <you@example.com>
2022-07-05 12:03:26 -0700

    git

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,17 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define ABS(x)  x
-
-int main(void)
-{
-	int x;
-
-	if (x < 0)
-	{
-		x = -1 * x
-	}
-	else
-	{
-		return (0);
-	}
-}
+#define ABS(x)  (x < 0 ? x *- 1 : x)
+#endif
